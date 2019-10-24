bbcef5c34044ad73ed633244691341684a8ddb7f
罗祥 <1366971433@qq.com>
2019-10-24 15:10:34 +0800

    modify

---
+++ code/Flink/flink-basis-java/src/main/java/com/heibaiying/BatchJob.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.heibaiying;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
+/**
+ * Skeleton for a Flink Batch Job.
+ *
+ * <p>For a tutorial how to write a Flink batch application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution,
+ * change the main class in the POM.xml file to this class (simply search for 'mainClass')
+ * and run 'mvn clean package' on the command line.
+ */
+public class BatchJob {
+
+    public static void main(String[] args) throws Exception {
+
+        final String rootPath = "D:\\BigData-Notes\\code\\Flink\\flink-basis-java\\src\\main\\resources\\";
+        // set up the batch execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSource<String> readTextFile = env.readTextFile(rootPath + "log4j.properties");
+        readTextFile.print();
+    }
+}
--- code/Flink/flink-basis-scala/src/main/scala/com/heibaiying/WordCountBatch.scala
+++ code/Flink/flink-basis-scala/src/main/scala/com/heibaiying/WordCountBatch.scala
@@ -6,7 +6,7 @@ object WordCountBatch {
 
   def main(args: Array[String]): Unit = {
     val benv = ExecutionEnvironment.getExecutionEnvironment
-    val text = benv.readTextFile("D:\\BigData-Notes\\code\\Flink\\flink-basis\\src\\main\\resources\\wordcount.txt")
+    val text = benv.readTextFile("D:\\BigData-Notes\\code\\Flink\\flink-basis-scala\\src\\main\\resources\\wordcount.txt")
     val counts = text.flatMap { _.toLowerCase.split(",") filter { _.nonEmpty } }.map { (_, 1) }.groupBy(0).sum(1)
     counts.print()
   }
